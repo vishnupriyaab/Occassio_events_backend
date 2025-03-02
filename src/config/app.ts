990b20aa90db085a337c17777ec7982d2047addd
vishnupriyaab <vishnupriyaotp2002@gmail.com>
2025-03-03 00:25:37 +0530

    node,mongo,cors setup is completed

---
+++ src/config/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const corsOptions = {
+  origin: "http://localhost:4200",
+  credentials: true,
+  methods: ["PUT", "GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "X-Requested-With",
+    "Accept",
+  ],
+};
+
+app.use(cors(corsOptions));
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express from "express";
+import { config } from "dotenv";
+import app from "./config/app";
+import connectDB from "./config/db";
 
-const app = express();
+connectDB();
+config();
+
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, Node.js with TypeScript!");
-});
-
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log("Server is runninggg...");
 });
