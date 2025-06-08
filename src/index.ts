647720f051d78c56bd6e3b29a7282285faf5971e
vishnupriyaab <vishnupriyaotp2002@gmail.com>
2025-06-08 21:12:51 +0530

    now i think the socket.io url is corrected

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL,
+    origin: 'https://api.occasio.sbs',
     methods: ["GET", "POST"],
     credentials: true
   }
