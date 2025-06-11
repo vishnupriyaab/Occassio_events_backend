8ae99a7904f5797388e3200d730d30730c1cb03f
vishnupriyaab <vishnupriyaotp2002@gmail.com>
2025-06-11 20:47:58 +0530

    token have change

--- src/controllers/management/userController/authController.ts
+++ src/controllers/management/userController/authController.ts
@@ -97,13 +97,13 @@ export class UserAuthController implements IUserAuthController {
 
       res
         .cookie("refresh_token", refreshToken, {
-          httpOnly: false,
+          httpOnly: true,
           secure: process.env.NODE_ENV === "production",
           sameSite: "strict",
           maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
         })
         .cookie("access_token", accessToken, {
-          httpOnly: false,
+          httpOnly: true,
           secure: process.env.NODE_ENV === "production",
           sameSite: "strict",
           maxAge: 24 * 60 * 60 * 1000, // 1 day
